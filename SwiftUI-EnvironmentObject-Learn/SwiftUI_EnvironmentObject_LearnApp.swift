9112c1426aedcc3ab8de81af8e1ca62f9f3efe1a
Subhrajyoti <subha.ionic3@gmail.com>
2020-10-24 14:46:32 +0530

    Initial Commit

---
+++ SwiftUI-EnvironmentObject-Learn/SwiftUI_EnvironmentObject_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EnvironmentObject_LearnApp.swift
+//  SwiftUI-EnvironmentObject-Learn
+//
+//  Created by Subhrajyoti Chakraborty on 24/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EnvironmentObject_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
